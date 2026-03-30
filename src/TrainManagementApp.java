e10801179ac0f1a0b0f46474d40ab5785275d364
Harsh-2803 <rupharsh07@gmail.com>
2026-03-30 14:30:36 +0530

    UC7: Sort bogies by capacity using Comparator

--- src/TrainManagementApp.java
+++ src/TrainManagementApp.java
@@ -1,16 +1,27 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+
+class Bogie {
+    String name;
+    int capacity;
+
+    Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+}
 
 public class TrainManagementApp {
     public static void main(String[] args) {
-        HashMap<String, Integer> map = new HashMap<>();
+        List<Bogie> list = new ArrayList<>();
 
-        map.put("Sleeper", 72);
-        map.put("AC Chair", 60);
-        map.put("First Class", 24);
+        list.add(new Bogie("Sleeper", 72));
+        list.add(new Bogie("AC Chair", 60));
+        list.add(new Bogie("First Class", 24));
 
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        list.sort(Comparator.comparingInt(b -> b.capacity));
+
+        for (Bogie b : list) {
+            System.out.println(b.name + " -> " + b.capacity);
         }
     }
 }
