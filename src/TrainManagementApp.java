c6fcc29eaadcf265ba80dfc606b422941fd3df1c
Harsh-2803 <rupharsh07@gmail.com>
2026-03-30 14:23:56 +0530

    UC3: Track unique bogie IDs using HashSet

--- src/TrainManagementApp.java
+++ src/TrainManagementApp.java
@@ -1,21 +1,15 @@
-import java.util.ArrayList;
+import java.util.HashSet;
 
-public class BogieManagement {
+public class UniqueBogieIDs {
     public static void main(String[] args) {
-        ArrayList<String> bogies = new ArrayList<>();
+        HashSet<String> bogieIDs = new HashSet<>();
 
-        bogies.add("Sleeper");
-        bogies.add("AC Chair");
-        bogies.add("First Class");
+        bogieIDs.add("B101");
+        bogieIDs.add("B102");
+        bogieIDs.add("B103");
+        bogieIDs.add("B101");
+        bogieIDs.add("B102");
 
-        System.out.println("After adding bogies: " + bogies);
-
-        bogies.remove("AC Chair");
-        System.out.println("After removing AC Chair: " + bogies);
-
-        boolean exists = bogies.contains("Sleeper");
-        System.out.println("Is Sleeper present? " + exists);
-
-        System.out.println("Final bogie list: " + bogies);
+        System.out.println(bogieIDs);
     }
 }
