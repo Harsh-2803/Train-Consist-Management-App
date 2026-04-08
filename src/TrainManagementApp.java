de92986427e9dd31fc02e261bf97d972da5a6712
Harsha Vardhan V <67772772+Harsh-2803@users.noreply.github.com>
2026-04-08 14:13:48 +0530

    Update TrainManagementApp.java

--- src/TrainManagementApp.java
+++ src/TrainManagementApp.java
@@ -4,7 +4,6 @@ public class TrainManagementApp {
 
     public static void main(String[] args) {
 
-        // Goods bogies: type and cargo
         List<String[]> bogies = new ArrayList<>();
 
         bogies.add(new String[]{"Cylindrical", "Petroleum"});
@@ -12,19 +11,16 @@ public class TrainManagementApp {
         bogies.add(new String[]{"Cylindrical", "Petroleum"});
         bogies.add(new String[]{"Box", "Coal"});
 
-        // Safety check using stream + allMatch
         boolean isSafe = bogies.stream().allMatch(bogie -> {
             String type = bogie[0];
             String cargo = bogie[1];
 
-            // Rule: Cylindrical → only Petroleum
             if (type.equals("Cylindrical")) {
                 return cargo.equals("Petroleum");
             }
             return true;
         });
 
-        // Output
         if (isSafe) {
             System.out.println("Train is Safety Compliant");
         } else {
